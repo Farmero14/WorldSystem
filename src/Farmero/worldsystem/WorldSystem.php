fc7070b9ef75424bb8d929345bdaa0148cd19879
Farmero14 <145240944+Farmero14@users.noreply.github.com>
2024-06-04 14:21:16 -0700

    Update WorldSystem.php

--- src/Farmero/worldsystem/WorldSystem.php
+++ src/Farmero/worldsystem/WorldSystem.php
@@ -5,12 +5,23 @@ declare(strict_types=1);
 namespace Farmero\worldsystem;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\world\generator\GeneratorManager;
 
 use Farmero\worldsystem\command\WorldCommand;
+use Farmero\worldsystem\generators\VoidGenerator;
+use Farmero\worldsystem\generators\FlatGenerator;
+use Farmero\worldsystem\generators\PMGenerator;
 
 class WorldSystem extends PluginBase {
 
     protected function onEnable(): void {
+        $this->registerGenerators();
         $this->getServer()->getCommandMap()->register("WorldSystem", new WorldCommand($this));
+    }
+
+    private function registerGenerators(): void {
+        GeneratorManager::addGenerator(VoidGenerator::class, "void");
+        GeneratorManager::addGenerator(FlatGenerator::class, "flat");
+        GeneratorManager::addGenerator(PMGenerator::class, "normal");
     }
 }
